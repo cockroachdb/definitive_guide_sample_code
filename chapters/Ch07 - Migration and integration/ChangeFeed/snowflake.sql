0ddc3cf4de010b76d6c7cacb81de170266fee7fa
Guy Harrison <guy.a.harrison@gmail.com>
2022-04-22 08:51:12 +1000

    updates

--- chapters/Ch07 - Migration and integration/ChangeFeed/snowflake.sql
+++ chapters/Ch07 - Migration and integration/ChangeFeed/snowflake.sql
@@ -2,8 +2,8 @@ CREATE DATABASE movrfeed;
 CREATE TABLE movr (changefeed_record VARIANT);
 
 CREATE STAGE cdc_stage url='s3://cockroachdefinitiveguide/movrFeed/' 
-    credentials=(aws_key_id='0N4RRX7VG5WQS0Y0DTG2' 
-    aws_secret_key='kLKkZjS/i98CN8oR6vp56lHC0ecgyf8zePCKd+aW') 
+    credentials=(aws_key_id='' 
+    aws_secret_key='kLKkZjS/+aW') 
     file_format = (type = json);
 
 CREATE PIPE cdc_pipe auto_ingest = TRUE as COPY INTO movr FROM @cdc_stage;
